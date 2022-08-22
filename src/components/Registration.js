6dc840390c099248c2b0832b7124d8292fee1d6b
Dolzhenkov-Andrii <oscurabow@gmail.com>
2022-08-23 00:50:30 +0300

    registration2

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,32 +1,27 @@
 import React from "react";
 import '../css/registration.css'
-
 function Registration() {
     return (
         <div className="containerRegistration">
-            <div className="containerReg_img">
-                <div className="containerReg_form">
-                    <div className="reg_form">
-                        <div className="reg_form_status">
-
+            {/* <div className="containerReg_img">
+                <div className="containerReg_img_slise"></div>
+            </div> */}
+            <div className="containerReg_form">
+                <div className="reg_form">
+                    <div className="reg_form_input">
+                        <div className="reg_form_input_title"><h1>REGISTRATION</h1>
                         </div>
-                        <div className="reg_form_input">
-                            <div className="reg_form_input_title"><h1>REGISTRATION</h1>
-                            </div>
-                            <div className="reg_form_input_columns">
-                                <input className="reg_form_input_style" type="text" username="username" required />
-                                <input className="reg_form_input_style" type="text" psw="psw" required />
-                                <input className="reg_form_input_style" type="text" psw2="psw2" required />
-                                <input className="reg_form_input_style" type="text" email="email" required />
-                                <input className="reg_form_input_style" type="text" phone="phone" required />
-                                <input className="reg_form_input_style" type="text" surname="surname" required />
-                                <input className="reg_form_input_style" type="text" name="name" required />
-                                <input className="reg_form_input_style" type="text" birthday="birthday" required />
-                            </div>
-                            <div className="reg_form_input_button">
-                                <button className="reg_form_input_button_reg_form_input_style" type="submit" value="registraitions">GO</button>
-                            </div>
+                        <div className="reg_form_input_columns">
+                            <input className="reg_form_input_style" minlength="6" maxlength="10" type="text" username="username" defaultValue="Your nikname" required />
+                            <input className="reg_form_input_style" type="password" psw="psw" defaultValue="password" required />
+                            <input className="reg_form_input_style" type="password" psw2="psw2" defaultValue="password2" required />
+                            <input className="reg_form_input_style" type="email" email="email" defaultValue="Your email" required />
+                            <input className="reg_form_input_style" type="text" phone="phone" defaultValue="Your phone" required />
+                            <input className="reg_form_input_style" type="text" surname="surname" defaultValue="Your surname" required />
+                            <input className="reg_form_input_style" type="text" name="name" defaultValue="Your name" required />
+                            <input className="reg_form_input_style" type="text" birthday="birthday" defaultValue="Your birthday" required />
                         </div>
+                        <button className="reg_form_input_button" type="submit" value="registraitions">done</button>
                     </div>
                 </div>
             </div>
